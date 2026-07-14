f0bc3b2bc8ab9d6477b4942e0184fa731edba426
NgsTks <24fi090@ms.dendai.ac.jp>
2026-07-14 18:03:50 +0900

    PATCH is now available

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ const corsOptions = {
     origin: "http://localhost:5173",
     credentials: true,
 
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"]
 };
 
